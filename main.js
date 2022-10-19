e9fb277dab72424a231da8563fab9e6c6bc0e44f
amzalac <abbyzalac@gmail.com>
2022-10-18 20:58:50 -0400

    Midterm commit

---
+++ main.js
@@ -0,0 +1,22 @@
+function goToProducts()
+{
+    location.href='index.html';
+}
+function productOneClick() {
+    location.href='productOneDetails.html';
+}
+function productTwoClick() {
+    location.href='productTwoDetails.html';
+}
+function productThreeClick() {
+    location.href='productThreeDetails.html';
+}
+function productFourClick() {
+    location.href='productFourDetails.html';
+}
+function productFiveClick() {
+    location.href='productFiveDetails.html';
+}
+function productSixClick() {
+    location.href='productSixDetails.html';
+}
